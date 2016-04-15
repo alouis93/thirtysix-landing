fb8cbef2442366a5bfab77fe13b9e1b16869699c
Andrew Louis <andrew.louis93@gmail.com>
2016-04-15 12:48:43 -0400

    PHP / Heroku

---
+++ index.php
@@ -0,0 +1,183 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>36 to Love - Available for iOS and Android</title>
+    <meta http-equiv="Content-Type" content="text/html charset=utf-8"/>
+  	<meta name="description" content="Fall in love in 36 questions. The 36 questions app that you've been waiting for - now available on iOS and Android platforms. Based on Dr. Arthur Aron's, and popularized by the New York Times article."/>
+  	<meta name="robots" content="index, follow" />
+  	<meta name="DC.title" content="36 to Love - Available for iOS and Android" />
+    <meta charset="utf-8" />
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <!-- CSS 
+        ================================================== -->
+    <!-- Bootstrap 3-->
+    <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+    <!-- Google Fonts -->
+    <link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,900,200italic,300italic,400italic,600italic,700italic,900italic|Roboto+Condensed:300italic,400italic,700italic,400,300,700' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Roboto+Condensed:300italic,400italic,700italic,400,300,700' rel='stylesheet' type='text/css'>
+    <!-- Template Styles -->
+    <link href="css/style.css" rel="stylesheet" media="screen">
+
+
+	<link rel="apple-touch-icon" sizes="57x57" href="icon/apple-icon-57x57.png">
+	<link rel="apple-touch-icon" sizes="60x60" href="icon/apple-icon-60x60.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="icon/apple-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="76x76" href="icon/apple-icon-76x76.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="icon/apple-icon-114x114.png">
+	<link rel="apple-touch-icon" sizes="120x120" href="icon/apple-icon-120x120.png">
+	<link rel="apple-touch-icon" sizes="144x144" href="icon/apple-icon-144x144.png">
+	<link rel="apple-touch-icon" sizes="152x152" href="icon/apple-icon-152x152.png">
+	<link rel="apple-touch-icon" sizes="180x180" href="icon/apple-icon-180x180.png">
+	<link rel="icon" type="image/png" sizes="192x192"  href="icon/android-icon-192x192.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="icon/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="96x96" href="icon/favicon-96x96.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="icon/favicon-16x16.png">
+	<link rel="manifest" href="icon/manifest.json">
+	<meta name="msapplication-TileColor" content="#ffffff">
+	<meta name="msapplication-TileImage" content="icon/ms-icon-144x144.png">
+	<meta name="theme-color" content="#ffffff">
+
+
+      </head>
+  <body>
+	  
+	  <!-- NAVBAR
+	      ================================================== -->
+	  <nav class="navbar navbar-default" role="navigation">
+	  	  <div class="container">
+			  <div class="navbar-header">
+			    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+			      <span class="sr-only">Toggle navigation</span>
+			      <span class="icon-bar"></span>
+			      <span class="icon-bar"></span>
+			      <span class="icon-bar"></span>
+			    </button>
+			    
+			    <!--Replace text with your app name or logo image-->
+			    <a class="navbar-brand" href="#"></a>
+			    
+			  </div>
+			  <div class="collapse navbar-collapse navbar-ex1-collapse">
+			    <ul class="nav navbar-nav">
+			      <li><a onclick="$('header').animatescroll({padding:71});">Start</a></li>
+			      <li><a onclick="$('.purchase').animatescroll({padding:71});">About</a></li>
+			      <li><a onclick="$('.get-it').animatescroll({padding:71});">Contact</a></li>
+			    </ul>
+			  </div>
+		  </div>
+	  </nav>
+	  
+	  
+	   <!-- HEADER
+	   ================================================== -->	  
+	  <header>
+		 <div class="container">
+			 <div class="row">
+				 <div class="col-md-12">
+					  <h1><span class="crim">36</span> to love*</h1>
+					  <p class="lead">*&nbsp;The developers are responsible for any feelings <span class='app_name'><span class='crim'>36</span> to love</span> may induce.</p>
+					  
+					  <div class="carousel-iphone">
+					  	<div id="carousel-example-generic" class="carousel slide">
+					    
+					    <!-- Indicators -->
+					    <ol class="carousel-indicators">
+					      <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+					      <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+					      <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+					    </ol>
+					  
+					    <!-- Wrapper for slides -->
+					    <div class="carousel-inner">
+					      <div class="item active">
+					        <img src="img/screenshots/app0.png" alt="App Screen 1">
+					      </div>
+					      <div class="item">
+					        <img src="img/screenshots/app1.png" alt="App Screen 2">
+					      </div>
+					      <div class="item">
+					        <img src="img/screenshots/app2.png" alt="App Screen 3">
+					      </div>
+					      
+					    </div>
+					  </div>
+					</div>
+				</div>	  
+			</div>    
+		</div>
+	 </header>
+	  
+	  
+	  <!-- PURCHASE
+	      ================================================== -->
+	  <section class="purchase">
+		  <div class="container">
+			  <div class="row">
+				  <div class="col-md-offset-2 col-md-8">
+					 <h1>Get Closer.</h1>
+		
+
+					 	    <p>Based on Dr. Arthur Aron's (SUNY) study, the app presents a series of thirty-six questions - each more probing than the last.</p>
+					 	    <p>Ideal for dates, and lazy evenings with your loved ones.</p>
+					 	    <hr/>
+					 	    <button type="button" onclick="location.href='https://itunes.apple.com/us/app/36-to-love/id979703234?ls=1&mt=8'" class="app-store"></button>	
+					 	    <button type="button" onclick="location.href='https://play.google.com/store/apps/details?id=com.andrewlouis.thirtysix'" class="android-store"></button>	
+				  </div>
+			  </div>
+		  </div>
+	  </section>
+	  
+
+	
+	
+
+	
+	 <!-- SOCIAL
+	     ================================================== -->
+	  <section class="social">
+	  	<div class="container">
+	  		  <div class="row">
+	  			  <div class="col-md-12">
+	  			  	<h2>Get the word out.</h2>
+	  			   	<a href="https://www.facebook.com/sharer/sharer.php?u=thirtysixtolove.me" target="_blank" class="icon-facebook"></a>
+	  			   	<a href="http://twitter.com/share?url=http://thirtysixtolove.me" target="_blank" class="icon-twitter"></a>
+<!-- 	  			   	<a class="icon-google"></a>
+	  			   	<a class="icon-instagram"></a>
+	  			   	<a class="icon-pinterest"></a> 
+-->
+	  			   </div>
+	  		  </div>
+	  	  </div>	  
+	  </section>
+	  
+	
+	 <!-- GET IT 
+	     ================================================== -->
+	  <section class="get-it">
+	  	<div class="container">
+	  		<div class="row">
+<!-- 	  			<div class="col-md-12">
+	  				<h1>Avaialable now on the App Store</h1>
+	  				<p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>
+	  				<button type="button" class="app-store"></button>
+	  			</div> -->
+	  			
+	  				<hr />
+		  			<ul>
+	                	<li><a href="mailto:a.louis@utoronto.ca?Subject=36%20to%20Love" target="_top">Contact</a></li>
+                	</ul>
+	  			
+	  		</div>
+	  	</div>
+	  </section>
+	  
+	 
+	 <!-- JAVASCRIPT
+	     ================================================== -->
+    <script src="js/jquery.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/animatescroll.js"></script>
+    <script src="js/scripts.js"></script>
+    <script src="js/retina-1.1.0.min.js"></script>
+  	
+</html>
